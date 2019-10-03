adbb9d0090a006ceea678433b1a0ea19583bf9d8
dhananjayharel <dhananjayharel@users.noreply.github.com>
2019-10-03 14:30:58 +0530

    Update replacetexts.sh

--- replacetexts.sh
+++ replacetexts.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-sed -i 's|"ng serve|"ng serve --poll=2000  --base-href=  --public-host=0.0.0.0/'${CONTAINERPATH}'_op --host 0.0.0.0 --port 4200 --disable-host-check|g' /home/project/$PROJECTDIR/package.json
+###sed -i 's|"ng serve|"ng serve --poll=2000  --base-href=  --public-host=0.0.0.0/'${CONTAINERPATH}'_op --host 0.0.0.0 --port 4200 --disable-host-check|g' /home/project/$PROJECTDIR/package.json
 sed -i 's|"ng serve"|"ng serve --poll=2000  --host 0.0.0.0 --port 4200 --disable-host-check"|g' /home/project/$PROJECTDIR/package.json
 ##sed -i 's|base href="/"|base href="/'${CONTAINERPATH}'_op/"|g' /home/project/$PROJECTDIR/src/index.html
 sed -i 's|src="./bundle.js"|src="http://d9n2pyny5ybdw.cloudfront.net/angularlatest_v4/bundle.js"|g' /home/theia/lib/index.html
